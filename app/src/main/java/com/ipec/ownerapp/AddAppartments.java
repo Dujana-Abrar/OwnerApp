72fa95b3fd3277fa11bf8bae5bef89075a5c336c
Dujana <thebuisnessd@gmail.com>
2021-03-29 17:10:57 +0530

    image upload done

--- app/src/main/java/com/ipec/ownerapp/AddAppartments.java
+++ app/src/main/java/com/ipec/ownerapp/AddAppartments.java
@@ -3,14 +3,18 @@ package com.ipec.ownerapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.media.MediaPlayer;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -21,29 +25,43 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
-import java.time.Duration;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
+import java.util.UUID;
 
 public class AddAppartments extends AppCompatActivity {
 
 
+    private static final int PICK_IMAGE = 1;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     private static final String TAG = "AddAppartments";
     List<String> sizes;
     Button add;
     String selectedSpinnerItem;
     Spinner spinner;
+    ImageView imageView;
+    Uri filePath;
+    FirebaseStorage storage;
+    StorageReference storageReference;
+    private String path;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_appartments);
+
+        // get the Firebase  storage reference
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
 
         sizes = new ArrayList<>();
         getListItems();
@@ -53,8 +71,77 @@ public class AddAppartments extends AppCompatActivity {
             {
                 addDataToDatabase();
             }});
+
+        imageView = findViewById(R.id.imageView2);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+                startActivityForResult(gallery, PICK_IMAGE);
+                }
+            });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE) {
+            filePath = data.getData();
+            imageView.setImageURI(filePath);
+        }
+    }
+
+
+    private void uploadImage()
+    {
+        if (filePath != null) {
+
+            // Code for showing progressDialog while uploading
+            ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Uploading Image");
+            progressDialog.show();
+
+            // Defining the child of storageReference
+
+            path = "images/" + UUID.randomUUID().toString();
+            StorageReference ref = storageReference.child(path);
+
+            // adding listeners on upload
+            // or failure of image
+            ref.putFile(filePath)
+                    .addOnSuccessListener(
+                            new OnSuccessListener<UploadTask.TaskSnapshot>() {
+
+                                @Override
+                                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot)
+                                {
+                                    progressDialog.dismiss();
+                                    Toast.makeText(AddAppartments.this, "Image Uploaded!!", Toast.LENGTH_LONG).show();
+                                }
+                            })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e)
+                        {
+                            progressDialog.dismiss();
+                            Toast.makeText(AddAppartments.this, "Failed " + e.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    })
+                    .addOnProgressListener(
+                            new OnProgressListener<UploadTask.TaskSnapshot>() {
+
+                                // Progress Listener for loading
+                                // percentage on the dialog box
+                                @Override
+                                public void onProgress(UploadTask.TaskSnapshot taskSnapshot)
+                                {
+                                    double progress
+                                            = (100.0 * taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
+                                    progressDialog.setMessage("Uploaded " + (int)progress + "%");
+                                }
+                            });
+        }
+    }
 
     public void getListItems()
     {
@@ -95,6 +182,9 @@ public class AddAppartments extends AppCompatActivity {
 
     }
 
+
+
+
     public void addDataToDatabase()
     {
         EditText TVname = findViewById(R.id.name);
@@ -115,10 +205,11 @@ public class AddAppartments extends AppCompatActivity {
         }
 
         else {
+            uploadImage();
             Map<String, Object> apartment = new HashMap<>();
             apartment.put("acc_name", name);
             apartment.put("address", address);
-            apartment.put("image", "qisi");
+            apartment.put("image", path);
             apartment.put("location", city);
             apartment.put("min_duration", "12");
             apartment.put("rent", rent);
